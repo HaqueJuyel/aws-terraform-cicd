e840f12789cb61f8548102b9b9917476d6740ffc
Mohammed Juyel Haque <mohammedjuyelh@gmail.com>
2025-08-24 01:08:05 +0530

    test github actions

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket1ju"
+    bucket         = "juyel_githubaction"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
